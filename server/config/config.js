e537adfc07a2f0a20d40494234f1983446c5be5b
Alberto <a-bedmarmto@outlook.com>
2020-06-01 15:04:31 +0200

    cambio en urlDB usando MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -18,8 +18,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cafe-user:28783728xX@cluster0-5jont.mongodb.net/cafe';
-    //urlDB = process.env.MONGO_URI;
+
+    urlDB = process.env.MONGO_URI;
 
 }
 process.env.URLDB = urlDB;
